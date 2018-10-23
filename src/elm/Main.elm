0815cdedba14877c80a09236e375488607a1c72e
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2018-10-23 00:56:32 -0300

    Refactor Main.elm: add a minimum setup for Browser.element

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,15 +1,59 @@
-module Main exposing (main, view)
+module Main exposing (main)
 
+import Browser
 import Html exposing (..)
-import Html.Attributes exposing (..)
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+-- MODEL
+
+
+type alias Model =
+    String
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+    ( "Titulo"
+    , Cmd.none
+    )
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        NoOp ->
+            ( model
+            , Cmd.none
+            )
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+-- VIEW
 
 
 view model =
-    div [ class "main" ]
-        [ h1 [] [ text "Titulo" ]
-        , p [] [ text "conteudo" ]
-        ]
-
-
-main =
-    view "foo"
+    h1 [] [ text model ]
