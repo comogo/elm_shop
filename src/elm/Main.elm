550f4fae9f5af86e2f1a6c666ee27c2ec9bc4d8f
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2018-10-25 11:13:29 -0300

    Add behaviour on hover a product in the list
    - Set cursor pointer on hover
    - Add style to add to cart text
    - Changed the Model to store the hovered product
    - Refactor update to handle onMouseOver 
      and onMouseOut events
    - Trigger event on onMouseOver and onMouseOut on
      product

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -39,12 +39,14 @@ type alias Product =
 
 
 type alias Model =
-    List Product
+    { products : List Product
+    , hoveredProduct : Maybe Product
+    }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Model productsMock
+    ( Model productsMock Nothing
     , Cmd.none
     )
 
@@ -55,6 +57,8 @@ init _ =
 
 type Msg
     = NoOp
+    | MouseOverProduct Product
+    | MouseOutProduct Product
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -62,6 +66,16 @@ update msg model =
     case msg of
         NoOp ->
             ( model
+            , Cmd.none
+            )
+
+        MouseOverProduct product ->
+            ( { model | hoveredProduct = Just product }
+            , Cmd.none
+            )
+
+        MouseOutProduct _ ->
+            ( { model | hoveredProduct = Nothing }
             , Cmd.none
             )
 
@@ -81,15 +95,54 @@ subscriptions model =
 
 {-| Renders a single Product as Html
 -}
-viewProduct : Product -> Html Msg
-viewProduct model =
-    div [ class "product" ]
-        [ div [ class "image-container" ] [ img [ src model.image ] [] ]
-        , div [ class "product__name" ] [ text model.name ]
-        , div [ class "product__price" ] [ text (Utils.currency model.price) ]
+viewProduct : Product -> Maybe Product -> Html Msg
+viewProduct model hovered =
+    let
+        image : Html Msg
+        image =
+            div [ class "image-container" ] [ img [ src model.image ] [] ]
+
+        completeInfo : List (Html Msg)
+        completeInfo =
+            [ div [ class "product__name" ] [ text model.name ]
+            , div
+                [ class "product__price" ]
+                [ text (Utils.currency model.price) ]
+            ]
+
+        addToCart : List (Html Msg)
+        addToCart =
+            [ div
+                [ class "product__add-to-cart" ]
+                [ text "adicionar ao carrinho" ]
+            ]
+
+        info : List (Html Msg)
+        info =
+            case hovered of
+                Nothing ->
+                    completeInfo
+
+                Just p ->
+                    if p.id == model.id then
+                        addToCart
+
+                    else
+                        completeInfo
+    in
+    div
+        [ class "product"
+        , onMouseOver (MouseOverProduct model)
+        , onMouseOut (MouseOutProduct model)
         ]
+        (image :: info)
 
 
 view : Model -> Html Msg
 view model =
-    div [ class "container" ] (List.map viewProduct model)
+    div
+        [ class "container" ]
+        (List.map
+            (\p -> viewProduct p model.hoveredProduct)
+            model.products
+        )
